ec11f3bb18c40ff6a99260a1de8511e7270ff6db
iTheCode <ithecode@outlook.com>
2016-10-28 01:56:55 -0500

    Corrección de nombres

--- Adopt-CSharp/Form1.Designer.cs
+++ Adopt-CSharp/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Login
+﻿namespace Adopt_CSharp
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtContraseña = new System.Windows.Forms.TextBox();
